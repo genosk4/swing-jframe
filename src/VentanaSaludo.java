8288a510529bbaf886371073d342299033984355
genosk4 <lincopidaniel7@gmail.com>
2025-09-05 02:01:57 -0400

    Boton de Limpiar añadido y configurado

--- src/VentanaSaludo.java
+++ src/VentanaSaludo.java
@@ -9,6 +9,8 @@ public class VentanaSaludo {
     private JTextField campoTexto;
     private JButton botonSaludar;
     private JLabel etiquetaSaludo;
+    private JButton botonLimpiar;
+
 
     public static void main(String[] args) {
         VentanaSaludo aplicacion = new VentanaSaludo();
@@ -25,7 +27,7 @@ public class VentanaSaludo {
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setLayout(null);
 
-        ventana.getContentPane().setBackground(new Color(220, 240, 255));
+        ventana.getContentPane().setBackground(new Color(177, 177, 177));
 
         campoTexto = new JTextField();
         campoTexto.setBounds(50, 30, 200, 25);
@@ -35,9 +37,13 @@ public class VentanaSaludo {
         botonSaludar = new JButton("Saludar");
         botonSaludar.setBounds(270, 30, 100, 25);
         botonSaludar.setFont(new Font("Inter", Font.BOLD, 14));
-
         etiquetaSaludo = new JLabel(" ");
         etiquetaSaludo.setBounds(50, 80, 300, 25);
+
+        botonLimpiar = new JButton("Limpiar");
+        botonLimpiar.setBounds(270, 80, 100, 25);
+        botonLimpiar.setFont(new Font("Inter", Font.BOLD, 14));
+
     }
 
     // Hace que el boton funcione, que se pueda usar
@@ -52,7 +58,6 @@ public class VentanaSaludo {
                 etiquetaSaludo.setText(usuario.getSaludo());
             }
         });
-
         campoTexto.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
@@ -60,12 +65,17 @@ public class VentanaSaludo {
                 }
             }
         });
+        botonLimpiar.addActionListener(e -> {
+            campoTexto.setText("");
+            etiquetaSaludo.setText(" ");
+        });
     }
 
     // agrega los botones, texto a la ventana
     public void agregarVentana() {
         ventana.add(campoTexto);
         ventana.add(botonSaludar);
+        ventana.add(botonLimpiar);
         ventana.add(etiquetaSaludo);
         ventana.setLocationRelativeTo(null);
         ventana.setVisible(true);
