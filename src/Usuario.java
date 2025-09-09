4c01950c621c041eb9140f9de4b2898bb5525905
genosk4 <lincopidaniel7@gmail.com>
2025-09-09 19:17:23 -0300

    clase Usuario modificada

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,5 +1,7 @@
 public class Usuario {
     private String nombre;
+    private String username;
+    private String password;
 
     public Usuario(String nombre) {
         this.nombre = nombre;
@@ -7,4 +9,19 @@ public class Usuario {
     public String getSaludo() {
         return "Hola: " + nombre;
     }
+    public Usuario(String username,String password, String nombre) {
+        this.username = username;
+        this.password = password;
+        this.nombre = nombre;
+    }
+
+    public boolean validarCredenciales(String u, String p) {
+        return this.username.equals(u) && this.password.equals(p);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
 }
